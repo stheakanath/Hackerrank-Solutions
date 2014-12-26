0eebb2fcd0c5bf40bd336b02bdef38875a2d68f6
Sony Theakanath <stheakanath@gmail.com>
2014-12-25 22:44:32 -0800

    find digits solution

---
+++ Algorithms/Warmup/findDigits.py
@@ -0,0 +1,10 @@
+cases = int(input())
+for i in range(cases):
+	result = 0
+	number = input()
+	n = int(number)
+	for a in range(len(number)):
+		if number[a] != "0":
+			if n % int(number[a]) == 0:
+				result += 1
+	print(result)
